338ef73aa462b4747b795897291239da9104289c
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-13 22:00:45 +0530

    Update largenum.py

--- largenum.py
+++ largenum.py
@@ -1,11 +1,9 @@
 def largest(n):
-    max=0
+    max=n[0]
     #print(n)
-    for i in range(len(n)-1):
-        if(n[i]>n[i+1]):
+    for i in range(len(n)):
+        if(n[i]>max):
             max=n[i]
-        else:
-            max=n[i+1]
     return max
 n1=input().split()
 lis=list(map(int,n1))
