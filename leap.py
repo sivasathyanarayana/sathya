dabdc139a2debad8f13cca7f259bdef15f71c1d9
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-13 22:59:52 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -8,10 +8,10 @@ year=int(input())
 if(year%4==0):
     if(year%100==0):
         if(year%400==0):
-            print("leap year")
+            print("yes")
         else:
-            print("not")
+            print("no")
     else:
-        print("leap year")
+        print("yes")
 else:
-    print("not")
+    print("no")
