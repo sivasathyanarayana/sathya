adf8391ad3002afe55e2f1b554718619325046f2
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-14 19:06:53 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 19:03:49 2019
+
+@author: Siva
+"""
+#Factorial of a number
+n=int(input())
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
