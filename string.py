86638289af38d9dd8e460a142afee0768038d7ca
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-15 21:41:54 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 21:34:34 2019
+
+@author: Siva
+"""
+#Given a string s and a number k,print the string k times
+lis=input().split()
+s=lis[0]
+k=int(lis[1])
+for i in range(k):
+    print(s)
