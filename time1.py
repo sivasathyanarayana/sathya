e40b80f01caded47a387c98a4721a7e368a8c4ef
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-15 19:10:46 +0530

    Create time1.py

---
+++ time1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 19:08:06 2019
+
+@author: Siva
+"""
+#Given time in minutes ,print it in hours and minutes\
+n=int(input())
+h=n//60
+m=n%60
+print(h,m)
