bdeee7f0424581499f3d605a542c3f9570d787d8
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-15 21:05:01 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 21:01:15 2019
+
+@author: Siva
+"""
+#program to swap two numbers
+n=list(map(int,input().split()))
+a=n[0]
+b=n[1]
+t=a
+a=b
+b=t
+print(a,b)
