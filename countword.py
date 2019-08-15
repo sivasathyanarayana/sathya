3ef1a03d285b7b9af3ccd62e5c03d629c5c7e675
sivasathyanarayana <41518358+sivasathyanarayana@users.noreply.github.com>
2019-08-15 19:30:56 +0530

    Update countword.py

--- countword.py
+++ countword.py
@@ -4,10 +4,13 @@ Created on Thu Aug 15 19:21:02 2019
 
 @author: Siva
 """
-#Count the charecters from the given string
+#Count the words from the given string
 n=input()
 c=1
-for i in n:
-    if(i==" "):
-        c=c+1
+if(len(n)<=1000):
+    for i in n:
+        if(i==" "):
+            c=c+1
+else:
+    print("invald")
 print(c)
